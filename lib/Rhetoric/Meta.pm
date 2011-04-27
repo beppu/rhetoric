da2eeb12cbd15637e382465091669596c60c48f8
John Beppu <john.beppu@gmail.com>
2011-04-27 00:54:50 -0700

    massive renaming

---
+++ lib/Rhetoric/Meta.pm
@@ -0,0 +1,10 @@
+package Rhetoric::Meta;
+use common::sense;
+use aliased 'Squatting::H';
+use Method::Signatures::Simple;
+
+# blog metadata and menus will always be in the filesystem
+our $meta = H->new({
+});
+
+1;
